c30186d55578ebef7a463aa32243f901cfa14906
slime <slime@users.noreply.github.com>
2025-12-04 14:02:54 +0800

    [rollout] support disable trim samples when converting rollout samples to train datas (#1016)

--- slime/ray/rollout.py
+++ slime/ray/rollout.py
@@ -158,7 +158,9 @@ class RolloutManager:
             while isinstance(data[0], list):
                 data = sum(data, [])
 
-            if len(data) % self.args.global_batch_size != 0:
+            if self.args.disable_rollout_trim_samples:
+                logger.info(f"Collectd {len(data)} samples from rollout to train")
+            elif len(data) % self.args.global_batch_size != 0:
                 trim_len = (len(data) // self.args.global_batch_size) * self.args.global_batch_size
                 origin_data_length = len(data)
                 data = data[:trim_len]
