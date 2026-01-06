4e7ef0b828f5001d96adccbd79bf0b51e9308f5f
slime <slime@users.noreply.github.com>
2026-01-06 11:17:34 +0800

    Patch validate_non_overlapping_shards_metadata to speed up ckpt loading (#1342)

--- slime/backends/megatron_utils/checkpoint.py
+++ slime/backends/megatron_utils/checkpoint.py
@@ -10,6 +10,85 @@ from megatron.training.global_vars import get_args
 
 from slime.utils import megatron_bridge_utils
 
+try:
+    # Here we patch out the `validate_non_overlapping_shards_metadata` in both functions
+    # because it is really slow for large models with many shards.
+    # TODO: find a less hacky way to do this.
+    import torch.distributed as dist
+    import torch.distributed._shard.sharding_spec as shard_spec
+    from torch.distributed._shard.sharded_tensor import ShardedTensor
+    from torch.distributed._shard.sharded_tensor.metadata import ShardedTensorMetadata
+    from torch.distributed._shard.sharded_tensor.shard import Shard
+    from torch.distributed._shard.sharded_tensor.utils import _parse_and_validate_remote_device
+    from torch.distributed._shard.sharding_spec.api import EnumerableShardingSpec
+
+    def __post_init__(self):
+        pass
+
+    EnumerableShardingSpec.__post_init__ = __post_init__
+
+    @classmethod
+    def _init_from_local_shards_and_global_metadata(  # type: ignore[override]
+        cls,
+        local_shards: list[Shard],
+        sharded_tensor_metadata: ShardedTensorMetadata,
+        process_group=None,
+        init_rrefs=False,
+        sharding_spec=None,
+    ) -> ShardedTensor:
+        """
+        Initialize a ShardedTensor with local shards and a global
+        ShardedTensorMetadata built on each rank.
+
+        Warning: This API is experimental and subject to change. It does
+                 not do cross rank validations, and fully rely on the user
+                 for the correctness of sharded_tensor_metadata on each rank
+        """
+        process_group = cls._normalize_pg(process_group)
+        current_rank = dist.get_rank()  # intentional to get global rank
+
+        shards_metadata = sharded_tensor_metadata.shards_metadata
+
+        local_shard_metadatas = []
+
+        # collect local shard metadatas from the global sharded_tensor_metadata
+        for shard_metadata in shards_metadata:  # type: ignore[attr-defined]
+            rank, local_device = _parse_and_validate_remote_device(process_group, shard_metadata.placement)
+
+            if current_rank == rank:
+                local_shard_metadatas.append(shard_metadata)
+
+        shards_metadata = sharded_tensor_metadata.shards_metadata
+        tensor_properties = sharded_tensor_metadata.tensor_properties
+
+        if sharding_spec is None:
+            spec = shard_spec._infer_sharding_spec_from_shards_metadata(shards_metadata)
+        else:
+            spec = sharding_spec
+
+        sharded_tensor = ShardedTensor.__new__(
+            ShardedTensor,
+            spec,
+            sharded_tensor_metadata.size,
+            dtype=tensor_properties.dtype,
+            layout=tensor_properties.layout,
+            pin_memory=tensor_properties.pin_memory,
+            requires_grad=tensor_properties.requires_grad,
+        )
+
+        # done validation, add local_shards
+        sharded_tensor._local_shards = local_shards
+        sharded_tensor._prepare_init(process_group=process_group, init_rrefs=init_rrefs)
+
+        # run post initialization, i.e. map registration, rpc initialization
+        sharded_tensor._post_init()
+        return sharded_tensor
+
+    ShardedTensor._init_from_local_shards_and_global_metadata = _init_from_local_shards_and_global_metadata
+
+except ImportError:
+    pass
+
 logger = logging.getLogger(__name__)
 
 __all__ = ["save_checkpoint"]
