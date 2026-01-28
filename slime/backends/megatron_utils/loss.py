fix compute logprobs menmory leak bug (#1506)

slime/backends/megatron_utils/loss.py
@@ -169,7 +169,7 @@ def get_log_probs_and_entropy(
     }
     if with_entropy:
         res["entropy"] = entropy_list
-    return res
+    return torch.empty((0,), device=logits.device), res
 
 
 def get_values(
@@ -214,7 +214,7 @@ def get_values(
         assert logits_chunk.size(-1) == 1, f"{logits_chunk.shape}"
         value_list.append(logits_chunk.squeeze(-1))
 
-    return {
+    return torch.empty((0,), device=logits.device), {
         "values": value_list,
     }
 
@@ -476,7 +476,7 @@ def policy_loss_function(
     total_lengths = batch["total_lengths"]
     max_seq_lens = batch.get("max_seq_lens", None)
 
-    log_probs_and_entropy = get_log_probs_and_entropy(
+    _, log_probs_and_entropy = get_log_probs_and_entropy(
         logits,
         args=args,
         unconcat_tokens=batch["unconcat_tokens"],
@@ -680,7 +680,7 @@ def value_loss_function(
     """
     old_values = torch.cat(batch["values"], dim=0)
 
-    values = get_values(
+    _, values = get_values(
         logits,
         args=args,
         unconcat_tokens=batch["unconcat_tokens"],
@@ -738,7 +738,7 @@ def sft_loss_function(
     response_lengths = batch["response_lengths"]
     total_lengths = batch["total_lengths"]
 
-    log_probs_and_entropy = get_log_probs_and_entropy(
+    _, log_probs_and_entropy = get_log_probs_and_entropy(
         logits,
         args=args,
         unconcat_tokens=batch["unconcat_tokens"],

tools/convert_hf_to_int4_direct.py
@@ -7,23 +7,18 @@ options:
 """
 
 import argparse
-import os
-import math
-import re
-
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-
 import gc
 import json
+import math
+import os
+import re
 import shutil
 import threading
 from concurrent.futures import ThreadPoolExecutor
 
 import safetensors
 import safetensors.torch
-
+import torch
 from tqdm import tqdm
 
 try:
@@ -123,7 +118,6 @@ def quantize(
     return output
 
 
-
 def pack_layer(weight, group_size, sym=True):
     w, scale, zp = fake_int4_quant_cuda.fake_int4_quant_cuda(weight, (1, group_size), sym)
     w = w.view(weight.shape[0], 1, weight.shape[1] // group_size, group_size)
@@ -169,15 +163,8 @@ class ConversionResult:
                 self.weight_map[k] = filename
                 self.param_count += len(v)
 
-def process_file(
-        input_path, 
-        output_path, 
-        filename, 
-        group_size,
-        is_symmetric, 
-        ignore_rules, 
-        result_collector
-    ):
+
+def process_file(input_path, output_path, filename, group_size, is_symmetric, ignore_rules, result_collector):
 
     print(f"Processing {filename}, memory usage: {torch.cuda.memory_allocated()}")
     weights = {}
@@ -186,7 +173,6 @@ def process_file(
     with safetensors.safe_open(os.path.join(input_path, filename), framework="pt", device="cuda") as f:
         for k in f.keys():
             weights[k] = f.get_tensor(k)
-
 
     for name, weight in weights.items():
         is_ignored = any(
@@ -215,7 +201,6 @@ def process_file(
 
     result_collector.add_result(filename, q_weights)
 
-    
 
 def convert_int4(input_path, output_path, group_size, is_symmetric, ignore_rules, max_workers):
     input_path = os.path.abspath(input_path)
@@ -230,19 +215,26 @@ def convert_int4(input_path, output_path, group_size, is_symmetric, ignore_rules
     # debug in single thread
     # for filename in safetensors_files:
     #     process_file(input_path, output_path, filename, group_size, is_symmetric, ignore_rules, result_collector)
-    
+
     # multi thread
     with ThreadPoolExecutor(max_workers=max_workers) as executor:
         futures = []
         for filename in safetensors_files:
             future = executor.submit(
-                process_file, input_path, output_path, filename, group_size, is_symmetric, ignore_rules, result_collector
+                process_file,
+                input_path,
+                output_path,
+                filename,
+                group_size,
+                is_symmetric,
+                ignore_rules,
+                result_collector,
             )
             futures.append(future)
 
         for future in tqdm(futures, desc="Processing files"):
             future.result()
-    
+
     quant_group = {
         "group_0": {
             "input_activations": None,
@@ -292,18 +284,24 @@ def parse_args():
     parser.add_argument("--save-dir", type=str, required=True)
     parser.add_argument("--group-size", type=int, default=32, help="Group Size")
     parser.add_argument("--is-symmetric", type=bool, default=True, help="Is Symmetric")
-    parser.add_argument("--ignore-rules", nargs="+", default=[
-        "re:.*lm_head.*",
-        "re:.*norm.*",
-        "re:.*embed.*",
-        "re:.*self_attn.*",
-        "re:.*shared_experts.*",
-        "re:.*mlp\\.(gate|up|gate_up|down)_proj.*",
-        "re:.*mlp\\.gate\\.*",
-    ], help="Ignore Rules")
+    parser.add_argument(
+        "--ignore-rules",
+        nargs="+",
+        default=[
+            "re:.*lm_head.*",
+            "re:.*norm.*",
+            "re:.*embed.*",
+            "re:.*self_attn.*",
+            "re:.*shared_experts.*",
+            "re:.*mlp\\.(gate|up|gate_up|down)_proj.*",
+            "re:.*mlp\\.gate\\.*",
+        ],
+        help="Ignore Rules",
+    )
     parser.add_argument("--max-workers", type=int, default=1, help="Number of worker threads for parallel processing")
 
     return parser.parse_args()
+
 
 def main():
     args = parse_args()
@@ -313,9 +311,12 @@ def main():
         os.makedirs(args.save_dir)
     elif not os.path.isdir(args.save_dir):
         raise ValueError("The save_dir should be a directory.")
-    
-    convert_int4(args.model_dir, args.save_dir, args.group_size, args.is_symmetric, args.ignore_rules, args.max_workers)
+
+    convert_int4(
+        args.model_dir, args.save_dir, args.group_size, args.is_symmetric, args.ignore_rules, args.max_workers
+    )
     print(f"Conversion complete, output saved to {args.save_dir}")
+
 
 if __name__ == "__main__":
     main()