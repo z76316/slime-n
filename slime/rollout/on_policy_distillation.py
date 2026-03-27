c57e13e493ea583523bfdfb426f218f1ce28c56e
slime <slime@users.noreply.github.com>
2026-03-27 15:44:21 +0800

    [Multimodal] Add Multimodal OPD support (#1760)

--- slime/rollout/on_policy_distillation.py
+++ slime/rollout/on_policy_distillation.py
@@ -1,6 +1,7 @@
 import aiohttp
 import torch
 
+from slime.utils.processing_utils import encode_image_for_rollout_engine
 from slime.utils.types import Sample
 
 
@@ -16,6 +17,11 @@ async def reward_func(args, sample, **kwargs):
         "return_logprob": True,
         "logprob_start_len": 0,
     }
+
+    if sample.multimodal_inputs and sample.multimodal_inputs.get("images"):
+        image_data = sample.multimodal_inputs["images"]
+        payload["image_data"] = [encode_image_for_rollout_engine(image) for image in image_data]
+
     session_kwargs = {}
     async with aiohttp.ClientSession(**session_kwargs) as session:
         async with session.post(args.rm_url, json=payload) as resp:
