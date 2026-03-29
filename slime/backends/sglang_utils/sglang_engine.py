b7959191db50e0336c9ac5c73184d03166ba41fa
slime <slime@users.noreply.github.com>
2026-03-29 13:47:17 +0800

    (fix):not have encoder_only attr cause run failed (#1741)

--- slime/backends/sglang_utils/sglang_engine.py
+++ slime/backends/sglang_utils/sglang_engine.py
@@ -51,7 +51,7 @@ def _to_local_gpu_id(physical_gpu_id: int) -> int:
 
 
 def launch_server_process(server_args: ServerArgs) -> multiprocessing.Process:
-    if server_args.encoder_only:
+    if hasattr(server_args, "encoder_only") and server_args.encoder_only:
         from sglang.srt.disaggregation.encode_server import launch_server
     else:
         from sglang.srt.entrypoints.http_server import launch_server
