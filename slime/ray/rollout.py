a41683cf8a217b1c9e21880885e7b6ee51bf4254
slime <slime@users.noreply.github.com>
2026-05-06 12:52:27 +0700

    Add missing metrics to log (#1890)

--- slime/ray/rollout.py
+++ slime/ray/rollout.py
@@ -1196,6 +1196,8 @@ def compute_metrics_from_samples(args, samples):
     log_dict = {}
     log_dict |= dict_add_prefix(compute_statistics(response_lengths), "response_len/")
     log_dict |= _compute_zero_std_metrics(args, samples)
+    log_dict |= _compute_spec_metrics(args, samples)
+    log_dict |= _compute_prefix_cache_metrics(args, samples)
     log_dict |= _compute_reward_cat_metrics(args, samples)
     log_dict["repetition_frac"] = np.mean([int(has_repetition(s.response)) for s in samples]).item()
     log_dict["truncated_ratio"] = np.mean([int(s.status == Sample.Status.TRUNCATED) for s in samples]).item()
